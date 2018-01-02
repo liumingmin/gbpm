82d9d0f3503c75fae516d90098d9e06ed3afeeff
liumingmin <443925902@qq.com>
2018-01-02 14:18:04 +0800

    comments..

---
+++ routers/router.go
@@ -0,0 +1,58 @@
+package routers
+
+import (
+	//"github.com/astaxie/beego"
+	//"tsrv/controllers"
+	//"tsrv/schedule"
+)
+
+func init() {
+
+//	beego.Router("/", &controllers.MainController{})
+//
+//	beego.Router("/upload", &controllers.UploadController{})
+//	beego.Router("/clientVersionInfo", &controllers.ClientVersionInfoController{})
+//	beego.Router("/clientVersionFile", &controllers.ClientVersionFileController{})
+//	beego.Router("/servVersionInfo", &controllers.ServVersionInfoController{})
+//
+//	beego.Router("/login", &controllers.AuthController{}, "get:ToLogin")
+//	beego.Router("/login", &controllers.AuthController{}, "post:Login")
+//	beego.Router("/logout", &controllers.AuthController{}, "post:Logout")
+//	beego.Router("/keeplive", &controllers.AuthController{}, "post:Keeplive")
+//	beego.Router("/keeplive/:token:string", &controllers.AuthController{}, "post:Keeplive")
+//	beego.Router("/mongoCfg", &controllers.AuthController{}, "post:GetMongoCfg")
+//
+//	beego.Router("/sql/exec", &controllers.SqlController{}, "post:SqlExec")
+//	beego.Router("/sql/query", &controllers.SqlController{}, "post:SqlQuery")
+//
+//	beego.Router("/data/ping", &controllers.DataController{}, "get:Ping")
+//	beego.Router("/data/metas", &controllers.DataController{}, "get:GetMetas")
+//
+//    beego.Router("/data/createdb/:projectId:string", &controllers.DataController{}, "post:CreateDB")
+//	beego.Router("/data/dropdb/:projectId:string", &controllers.DataController{}, "post:DropDB")
+//
+//	beego.Router("/data/post", &controllers.DataController{}, "post:PostModels")
+//	beego.Router("/data/post/:projectId:string", &controllers.DataController{}, "post:PostModels")
+//
+//	beego.Router("/data/get", &controllers.DataController{}, "post:GetModels")
+//
+//	beego.Router("/data/file/:fileId:string", &controllers.DataController{}, "get:GetModelsFile")
+//
+//	beego.Router("/notify/send", &controllers.NotifyController{}, "post:SendNotify")
+//
+//	beego.Router("/restapi/createdb/:projectId:string", &controllers.DataController{}, "post:CreateDB")
+//	beego.Router("/restapi/dropdb/:projectId:string", &controllers.DataController{}, "post:DropDB")
+//
+//	beego.Router("/restapi/get/:modelName:string/:projectId:string", &controllers.RestApiController{}, "post:RetrievEntity")
+//	beego.Router("/restapi/update/:modelName:string/:projectId:string", &controllers.RestApiController{}, "post:UpdateEntity")
+//	beego.Router("/restapi/delete/:modelName:string/:projectId:string", &controllers.RestApiController{}, "post:DeleteEntity")
+//	beego.Router("/restapi/add/:modelName:string/:projectId:string", &controllers.RestApiController{}, "post:CreateEntity")
+//
+//	beego.Router("/restapi/list/:modelName:string/:projectId:string", &controllers.RestApiController{}, "post:GetEntities")
+//	beego.Router("/restapi/query/:modelName:string/:projectId:string", &controllers.RestApiController{}, "post:QueryEntities")
+//
+//	beego.Router("/restapi/sync/:modelName:string/:projectId:string", &controllers.RestApiController{}, "post:SyncEntities")
+
+}
+
+
